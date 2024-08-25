0e8c4f2062908b0dbd27c47edbf4285927277f89
Sivagami34 <ksivagami2u@gmail.com>
2024-08-25 09:14:37 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -12,10 +12,17 @@ class Stack:
         else:
             print("stack overflow")
     def pop(self):
-        if len(self.stack) != 0: 
+        if not self.isempty(): 
             self.stack.pop()
         else:
             print("stack underflow")
+    def top(self):
+        return self.stack[len(self.stack)-1]
+    def isempty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
 
 fruit = Stack(3)
 fruit.display()
@@ -23,9 +30,11 @@ fruit.push("kiwi")
 fruit.display()
 fruit.push("lemon")
 fruit.display()
+print(fruit.top())
 fruit.push("apple")
 fruit.display()
 fruit.push("banana")
 fruit.display()
 fruit.pop()
 fruit.display()
+print(fruit.top())
