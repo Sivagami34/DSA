33ac0bb7b3517745a6e0cbf984a0850601f0cbb1
Sivagami34 <ksivagami2u@gmail.com>
2024-10-06 10:00:22 +0100

    Update bst.py

--- bst.py
+++ bst.py
@@ -1,3 +1,4 @@
+
 class Node():
     def __init__(self, value):
         self.key = value
@@ -20,9 +21,46 @@ def insert(root, newvalue):
         root.left = insert(root.left, newvalue)
     return root
 
+def search(root, value):
+    if root.key == value:
+        return True
+    elif root.key < value and root.right is not None:
+        return search(root.right, value)
+    elif root.key > value and root.right is not None:
+        return search(root.left, value)
+    else:
+        return False
+    
+def insucc(nnode):
+    succ = nnode.right
+    while succ.left is not None:
+        succ = succ.left
+    return succ
+
+def delete(root, value):
+    if root == None:
+        return None
+    if root.key < value:
+        root.right = delete(root.right, value)
+    elif root.key > value:
+        root.left = delete(root.left, value)
+    else:
+        if root.right is None:
+            return root.left
+        if root.left is None:
+            return root.right
+        suc = insucc(root)
+        root.key = suc.key
+        root.right = delete(root.right, suc.key)
+    return root
+
 nnodes = int(input("How many nodes would you like to insert? "))
 root = None
 for i in range(nnodes):
     val = int(input("Enter a value: "))
     root = insert(root, val)
+inorder(root)
+print(search(root,8))
+d = int(input("what node would you like to delete: "))
+delete(root, d)
 inorder(root)
